f9d33d51d876a8c22db94ab26089c15ffb1a4635
ILONA <ilonazelenaya@gmail.com>
2020-10-10 18:12:19 +0200

    correct some mistake

--- js/main.js
+++ js/main.js
@@ -1,59 +1,50 @@
 'use strict';
 const TITLE = [
-  `Уютное жилье`,
-  `Место, которое вы давно искали`,
-  `Жилье с потрясающим видом из окна`,
-  `Вы обязательно к нам вернетесь!`,
-  `Уютное жилье`,
-  `Место, которое вы давно искали`,
-  `Жилье с потрясающим видом из окна`,
-  `Вы обязательно к нам вернетесь!`,
+  'Уютное жилье',
+  'Место, которое вы давно искали',
+  'Жилье с потрясающим видом из окна',
+  'Вы обязательно к нам вернетесь!',
+  'Уютное жилье',
+  'Место, которое вы давно искали',
+  'Жилье с потрясающим видом из окна',
+  'Вы обязательно к нам вернетесь!',
 ];
-const AVATARS = [
-  `img/avatars/user01.png`,
-  `img/avatars/user02.png`,
-  `img/avatars/user03.png`,
-  `img/avatars/user04.png`,
-  `img/avatars/user05.png`,
-  `img/avatars/user06.png`,
-  `img/avatars/user07.png`,
-  `img/avatars/user08.png`,
-];
+
 const TYPE_APARTMENT = [
-  `palace`,
-  `flat`,
-  `house`,
-  `bungalow`,
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
 ];
 const CHECHKIN = [
-  `12:00`,
-  `13:00`,
-  `14:00`,
+  '12:00',
+  '13:00',
+  '14:00',
 ];
 const CHECHKOUT = [
-  `12:00`,
-  `13:00`,
-  `14:00`,
+  '12:00',
+  '13:00',
+  '14:00',
 ];
 const FEATURES = [
-  `wi-fi`,
-  `dishwasher`,
-  `parking`,
-  `washer`,
-  `elevator`,
-  `conditioner`,
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
 ];
 const PHOTOS = [
-  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 const MAP_RANGE_TOP = 130;
 const MAP_RANGE_BOTTOM = 630;
 const APP_COUNTS = 8;
-const mapPins = document.querySelector(`.map__pins`);
-const mapWidth = (document.querySelector(`.map__overlay`).offsetWidth);
-const teplatePin = document.querySelector(`#pin`).content.querySelector(`button`);
+const mapPins = document.querySelector('.map__pins');
+const mapWidth = (document.querySelector('.map__overlay').offsetWidth);
+const teplatePin = document.querySelector('#pin').content.querySelector('button');
 const appartmens = generateAppartments();
 
 function getRandomNumber(min, max) {
@@ -65,7 +56,7 @@ function randomElementArray(array) {
 }
 
 function generateArray(array) {
-  var arr = [];
+  let arr = [];
   for (let i = 0; i < getRandomNumber(0, array.length); i++) {
     arr[i] = randomElementArray(array);
   }
@@ -78,7 +69,7 @@ function generateAppartments() {
     let obj = {};
     obj = {
       author: {
-        avatar: AVATARS[i]
+        avatar: `img/avatars/user0${i + 1}.png`
       },
       offer: {
         title: randomElementArray(TITLE),
@@ -90,7 +81,7 @@ function generateAppartments() {
         checkin: randomElementArray(CHECHKIN),
         checkout: randomElementArray(CHECHKOUT),
         features: generateArray(FEATURES),
-        description: `Описание выбранного жилья`,
+        description: 'Описание выбранного жилья',
         photos: generateArray(PHOTOS)
       },
       location: {
@@ -105,7 +96,7 @@ function generateAppartments() {
 
 function renderPin(pin) {
   let pinElement = teplatePin.cloneNode(true);
-  let pinImg = pinElement.querySelector(`img`);
+  let pinImg = pinElement.querySelector('img');
   pinImg.src = pin.author.avatar;
   pinImg.alt = pin.offer.title;
   pinElement.style.left = `${pin.location.x - pinElement.offsetWidth}px`;
@@ -114,9 +105,13 @@ function renderPin(pin) {
   return pinElement;
 }
 
-document.querySelector(`.map`).classList.remove(`map--faded`);
-const fragment = document.createDocumentFragment();
-for (let i = 0; i < appartmens.length; i++) {
-  fragment.appendChild(renderPin(appartmens[i]));
+function appendPin() {
+  document.querySelector('.map').classList.remove('map--faded');
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < appartmens.length; i++) {
+    fragment.appendChild(renderPin(appartmens[i]));
+  }
+  mapPins.appendChild(fragment);
 }
-mapPins.appendChild(fragment);
+
+appendPin();
